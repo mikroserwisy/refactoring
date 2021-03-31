963721838f44c141c1f1e11bea0c466f976a6213
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2021-03-31 11:42:49 +0200

    JUnit

--- src/main/java/pl/training/shop/payments/adapters/input/api/PaymentDto.java
+++ src/main/java/pl/training/shop/payments/adapters/input/api/PaymentDto.java
@@ -1,4 +1,13 @@
 package pl.training.shop.payments.adapters.input.api;
 
+import lombok.Data;
+
+import java.time.Instant;
+
+@Data
 public class PaymentDto {
+
+    private String id;
+    private Instant timestamp;
+
 }
--- src/main/java/pl/training/shop/payments/adapters/input/api/RestApplication.java
+++ src/main/java/pl/training/shop/payments/adapters/input/api/RestApplication.java
@@ -1,7 +1,8 @@
 package pl.training.shop.payments.adapters.input.api;
 
 import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
 
 @ApplicationPath("api/v1")
-public class RestConfiguration {
+public class RestApplication extends Application {
 }
