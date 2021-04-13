2524d31930bbb88718c76e5fc97a20650314c2cc
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2021-04-13 14:52:15 +0200

    unit

--- src/main/java/pl/training/shop/payments/Payments.java
+++ src/main/java/pl/training/shop/payments/Payments.java
@@ -1,5 +1,8 @@
 package pl.training.shop.payments;
 
+import javax.ejb.Local;
+
+@Local
 public interface Payments {
 
     Payment process(PaymentRequest paymentRequest);
--- src/main/java/pl/training/shop/payments/UUIDPaymentIdGenerator.java
+++ src/main/java/pl/training/shop/payments/UUIDPaymentIdGenerator.java
@@ -1,7 +1,9 @@
 package pl.training.shop.payments;
 
+import javax.ejb.Singleton;
 import java.util.UUID;
 
+@Singleton
 public class UUIDPaymentIdGenerator implements PaymentIdGenerator {
 
     public String getNext() {
