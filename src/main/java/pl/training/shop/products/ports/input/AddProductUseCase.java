55e163f11a01ea0e874a51cdeb59be0357292ea5
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2021-04-14 06:33:11 +0200

    update

--- src/main/java/pl/training/shop/products/ports/input/AddProductUseCase.java
+++
@@ -1,9 +0,0 @@
-package pl.training.shop.products.ports.input;
-
-import pl.training.shop.products.domain.Product;
-
-public interface AddProductUseCase {
-
-    Product add(Product product);
-
-}
--- src/main/java/pl/training/shop/products/ports/output/persistence/ProductUpdates.java
+++
@@ -1,9 +0,0 @@
-package pl.training.shop.products.ports.output.persistence;
-
-import pl.training.shop.products.domain.Product;
-
-public interface ProductUpdates {
-
-    Product save(Product product);
-
-}
